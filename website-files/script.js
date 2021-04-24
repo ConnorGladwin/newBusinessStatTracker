c17a1aa14f152a60071597d2b3e41462dc7bc086
ConnorGladwin <Cygladwin795@gmail.com>
2021-04-24 22:27:03 +0200

    Functionality
    - Fixed input NaN error and collapsing field error
    - added functions for payouts, updates and calls

--- website-files/script.js
+++ website-files/script.js
@@ -1,63 +1,77 @@
 // TODO
-// fix the in/out fields to work with the checkZero function
+// create total action calc function
 
 // call variables 
 let call = document.getElementById('call').value;
 let callSub = document.getElementById('callSub').value;
 let callAdd = document.getElementById('callAdd').value;
 
+let input;
+
+let updateTotal = 0;
+let callTotal = 0;
 let total = 0;
 
 // output contract value
-function contractChange(total) {
-  let contract = document.getElementById('contract').value;
-  document.querySelector('.wContract').textContent = contract;
-  
-  checkZero(contract);
+function contractChange() {
+  input = parseInt(document.getElementById('contract').value);
+  document.querySelector('.wContract').textContent = checkZero(input); 
 }
 
 // output payout pending value
 function pPendingChange() { 
-  const pPending = document.getElementById('pPayout').value;
-  const output = document.querySelector('.wPayoutPending');
-  
-  output.textContent = pPending;
-
-  checkZero(pPending, output);
+  input = parseInt(document.getElementById('pPayout').value);
+  document.querySelector('.wPending').textContent = checkZero(input);
 }
 
 // output payout value
 function payoutChange() {
-  let payout = document.getElementById('payout').value;
-  document.querySelector('.wPayout').textContent = payout;
-
-  checkZero(payout)
+  input = parseInt(document.getElementById('payout').value);
+  document.querySelector('.wPayout').textContent = checkZero(input);
 }
 
 // output calculated update value
 function updateChange() {
-  let mUpdate = document.getElementById('mUpdate').value;
-  let pUpdate = document.getElementById('pUpdate').value;
-  let updateTotal = mUpdate + pUpdate;
+  let m = parseInt(document.getElementById('mUpdate').value) / 3;
+  let p = parseInt(document.getElementById('pUpdate').value) / 3;
 
-  document.querySelector('.wUpdates').textContent = updateTotal;
+  if (Number.isNaN(p)) {
+    p = 0;
+  } 
+  
+  if (Number.isNaN(m)) {
+    m = 0;
+  }
 
-  checkZero(mUpdate);
-  checkZero(pUpdate);
+  // calculates the ceiling of the two values when added
+  // and displays output
+  document.querySelector('.wUpdates').textContent = Math.ceil(m + p);
 }
 
 // output calculated call value
 function callChange() {
+  let call = parseInt(document.getElementById('call').value);
+  let add = parseInt(document.getElementById('callAdd').value);
+  let sub = parseInt(document.getElementById('callSub').value);
 
+  if (Number.isNaN(add)) {
+    add = 0;
+  }
+  if (Number.isNaN(sub)) {
+    sub = 0;
+  }
+
+  let total = (call + add) - sub;
+  document.querySelector('.calcCalls').textContent = total;
+  document.querySelector('.wCalls').textContent = Math.ceil(total / 3);
 }
 
-// TODO
-// Check the output and either return a number
+// Checks the output and either returns a number
 // value or zero which will be displayed in the output
-function checkZero(input, output) {
-  if (input === ' ') {
-    return output.textContent = 0;
-  } 
-
-  console.log(output);
+function checkZero(input) {
+  if (Number.isNaN(input)) {
+    return input = 0;
+  } else {
+    return input = input;
+  }
 }
