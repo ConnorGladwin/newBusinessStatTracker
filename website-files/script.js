8dd1c7c83af2f33bb946cff31d13bb9d79d99b88
Connor Gladwin <Cygladwin795@gmail.com>
2021-04-25 18:30:01 +0200

    Total calc and set/get storage
    - added function to add values and display total actions
    - added functionality to add input values to local storage
    - added functionality to get values from local storage, automatically add them to the correct input field, then run update functions on each field

--- website-files/script.js
+++ website-files/script.js
@@ -1,33 +1,34 @@
 // TODO
-// create total action calc function
+// Set totals to local storages
 
-// call variables 
-let call = document.getElementById('call').value;
-let callSub = document.getElementById('callSub').value;
-let callAdd = document.getElementById('callAdd').value;
-
-let input;
-
-let updateTotal = 0;
-let callTotal = 0;
-let total = 0;
+let wContract = 0;
+let wPending = 0;
+let wPayout = 0;
+let wUpdate = 0;
+let wCalls = 0;
 
 // output contract value
 function contractChange() {
   input = parseInt(document.getElementById('contract').value);
-  document.querySelector('.wContract').textContent = checkZero(input); 
+  document.querySelector('.wContract').textContent = checkZero(input);
+  setStorage('contract', input);
+  return (wContract = input);
 }
 
 // output payout pending value
-function pPendingChange() { 
-  input = parseInt(document.getElementById('pPayout').value);
+function pendingChange() {
+  input = parseInt(document.getElementById('pending').value);
   document.querySelector('.wPending').textContent = checkZero(input);
+  setStorage('pending', input);
+  return (wPending = input);
 }
 
 // output payout value
 function payoutChange() {
   input = parseInt(document.getElementById('payout').value);
   document.querySelector('.wPayout').textContent = checkZero(input);
+  setStorage('payout', input);
+  return (wPayout = input);
 }
 
 // output calculated update value
@@ -37,20 +38,23 @@ function updateChange() {
 
   if (Number.isNaN(p)) {
     p = 0;
-  } 
-  
+  }
+
   if (Number.isNaN(m)) {
     m = 0;
   }
 
   // calculates the ceiling of the two values when added
   // and displays output
-  document.querySelector('.wUpdates').textContent = Math.ceil(m + p);
+  document.querySelector('.wUpdates').textContent = checkZero(Math.ceil(m + p));
+  setStorage('mUpdates', m);
+  setStorage('pUpdates', p);
+  return (wUpdate = Math.ceil(m + p));
 }
 
 // output calculated call value
 function callChange() {
-  let call = parseInt(document.getElementById('call').value);
+  const call = parseInt(document.getElementById('call').value);
   let add = parseInt(document.getElementById('callAdd').value);
   let sub = parseInt(document.getElementById('callSub').value);
 
@@ -61,17 +65,45 @@ function callChange() {
     sub = 0;
   }
 
-  let total = (call + add) - sub;
+  const total = call + add - sub;
   document.querySelector('.calcCalls').textContent = total;
-  document.querySelector('.wCalls').textContent = Math.ceil(total / 3);
+  document.querySelector('.wCalls').textContent = checkZero(
+    Math.ceil(total / 3)
+  );
+  setStorage('call', call);
+  setStorage('add', add);
+  setStorage('sub', sub);
+  return (wCalls = Math.ceil(total / 3));
+}
+
+function calcTotal() {
+  const total = wContract + wPending + wPayout + wUpdate + wCalls;
+  document.querySelector('.wTotal').textContent = checkZero(total);
 }
 
 // Checks the output and either returns a number
 // value or zero which will be displayed in the output
 function checkZero(input) {
   if (Number.isNaN(input)) {
-    return input = 0;
-  } else {
-    return input = input;
+    return (input = 0);
   }
+  return (input = input);
 }
+
+function setStorage(key, value) {
+  localStorage.setItem(key, value);
+}
+
+function getStorage() {
+  document.getElementById('contract').value = localStorage.getItem('contract');
+  contractChange();
+  document.getElementById('pending').value = localStorage.getItem('pending');
+  pendingChange();
+  document.getElementById('payout').value = localStorage.getItem('payout');
+  payoutChange();
+  document.getElementById('mUpdate').value = localStorage.getItem('mUpdate');
+  document.getElementById('pUpdate').value = localStorage.getItem('pUpdate');
+  updateChange();
+}
+
+window.onload = getStorage();
