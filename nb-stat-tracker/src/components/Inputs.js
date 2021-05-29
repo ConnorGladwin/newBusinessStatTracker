be6e1270330365b200d8c4510cbcbb984e711cc2
ConnorGladwin <Cygladwin795@gmail.com>
2021-05-29 20:58:07 +0200

    Created input and output components
    - Created separate  components for the input and output and added them to the render method of the Main component

--- nb-stat-tracker/src/components/Inputs.js
+++ nb-stat-tracker/src/components/Inputs.js
@@ -1,12 +1,83 @@
 // TODO
-// Rewrite input render here
+// Add calc functionality
 
 import React from 'react'
 
 class Inputs extends React.Component {
   render() {
     return (
-      <p>Inputs go here</p>
+      // Input fields
+      <div className="mx-auto">
+        <div className="flex flex-col items-center h-full">
+          <div>
+            {/* NBO */}
+            <div className="flex border-2 border-black rounded-md mb-2 px-1 py-2">
+              {/* Apps */}
+              <div className="mx-2">
+                <label htmlFor="apps" className="flex justify-start mb-2">Applications</label>
+                <input type="number" name="apps" id="apps" className="border-2 border-black rounded-md" placeholder="0" />
+              </div>
+              {/* Contracts */}
+              <div className="mx-2">
+                <label htmlFor="contract" className="flex justify-start mb-2">Contracts</label>
+                <input type="number" name="contract" id="contract" className="border-2 border-black rounded-md" placeholder="0" />
+              </div>
+            </div>
+            {/* Payouts */}
+            <div className="flex border-2 border-black rounded-md mb-2 px-1 py-2">
+              {/* Pending */}
+              <div className="mx-2">
+                <label htmlFor="pending" className="flex justify-start mb-2">Payout Pending</label>
+                <input type="number" name="pending" id="pending" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+              {/* Payout */}
+              <div className="mx-2">
+                <label htmlFor="payout" className="flex justify-start mb-2">Payout</label>
+                <input type="number" name="payout" id="payout" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+            </div>
+          </div>
+          {/* General Stats */} 
+          <div className="mx-2">
+            {/* Updates */}
+            <div className="flex border-2 border-black rounded-md mb-2 px-1 py-2">
+              <div className="mx-2">
+                <label htmlFor="mUpadte" className="flex justify-start mb-2">Manual Update</label>
+                <input type="number" name="mUpdate" id="mUpdate" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+              <div className="mx-2">
+                <label htmlFor="pUpdate" className="flex justify-start mb-2">Podium Update</label>
+                <input type="number" name="pUpdate" id="pUpdate" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+            </div>
+            {/* Calls */}
+            <div className="flex mb-2">
+              <div className="">
+                <label htmlFor="calls" className="flex justify-start mb-2">Calls</label>
+                <input type="number" name="calls" id="calls" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+              <div className="w-full mx-2">
+                <label htmlFor="calcCalls" className="flex justify-start mb-2">Calculated Calls</label>
+                <p className="border-2 border-black rounded-md text-center">0</p>
+              </div>
+            </div>
+          </div>
+          {/* Call modifiers */}
+          <div className="flex flex-col border-2 border-black rounded-md mb-2 px-1 py-2">
+            <label htmlFor="callMods">Call Modifiers</label>
+            <div className="flex">
+              <div className="mx-2">
+                <label htmlFor="callSub" className="flex justify-start mb-2">Subtract</label>
+                <input type="number" name="callSub" id="callSub" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+              <div className="mx-2">
+                <label htmlFor="callAdd" className="flex justify-start mb-2">Add</label>
+                <input type="number" name="callAdd" id="callAdd" className="border-2 border-black rounded-md mb-2" placeholder="0" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
