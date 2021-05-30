2cb580964b2fc8d8a6a90c81cf5d2edd72356988
Connor Gladwin <Cygladwin795@gmail.com>
2021-05-30 22:10:48 +0200

    Added react files (#1)
    * Added react files
    
    Refactored ui into react
    
    * Created new components
    
    - Created the input and output components that the Main component will be broken up into
    - Added function for changing output state
    
    * Created input and output components
    
    - Created separate  components for the input and output and added them to the render method of the Main component
    
    * Added functionality
    
    - Added localStorage clear function
    - Added output display to applications and contracts

---
+++ nb-stat-tracker/src/App.js
@@ -0,0 +1,19 @@
+// deps imports
+import './App.css';
+import 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// component imports
+import Navbar from './components/Navbar';
+import Main  from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar  />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ nb-stat-tracker/src/components/Outputs.js
@@ -0,0 +1,53 @@
+// TODO
+// Display all weighted totals
+
+import React from 'react';
+
+class Outputs extends React.Component {
+  render() {
+    return (
+      <div className="mx-auto">
+        <div className="flex flex-col items-center justify-center h-full">
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="wApps" className="flex justify-start mb-2">Applications</label>
+              <p className="wApps border-2 border-black rounded-md text-center my-2">{this.props.apps}</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="wContract" className="flex justify-start mb-2">Contracts</label>
+              <p className="wContract border-2 border-black rounded-md text-center my-2">{this.props.contract}</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="payout" className="flex justify-start mb-2">Payouts</label>
+              <p className="wPayout border-2 border-black rounded-md text-center my-2">0</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="update" className="flex justify-start mb-2">Updates</label>
+              <p className="wUpdates border-2 border-black rounded-md text-center my-2">0</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="calls" className="flex justify-start mb-2">Calls</label>
+              <p className="wCalls border-2 border-black rounded-md text-center my-2">0</p>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="">
+              <label htmlFor="total" className="flex justify-start mb-2">Total Actions</label>
+              <p className="total border-2 border-black rounded-md text-center my-2">0</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Outputs;
